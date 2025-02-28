51ebcca23b35a211a6d1262cf7f87836cc6900cc
snigdho48 <www.atik48@gmail.com>
2025-02-28 19:43:04 +0600

    Initial commit

--- api/view/Releaseapp.py
+++ api/view/Releaseapp.py
@@ -3,11 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from api.models import ReleaseApp
-from api.serializer import ReleaseAppSerializer
 
 class ReleaseAppViewSet(APIView):
     
     def get(self, request):
         apps = ReleaseApp.objects.all().order_by('-created_at').first()
-        apps.c
+        apps.count += 1
+        apps.save()
         return Response({'url':apps.app}, status=status.HTTP_200_OK)
