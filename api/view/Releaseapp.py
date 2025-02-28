49714bd47eb7b13d18847c825773e1ddffbd49b1
snigdho48 <www.atik48@gmail.com>
2025-02-28 20:26:46 +0600

    Initial commit

--- api/view/Releaseapp.py
+++ api/view/Releaseapp.py
@@ -1,13 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 from api.models import ReleaseApp
 
 class ReleaseAppViewSet(APIView):
-    
+    permission_classes = [AllowAny]  
+
     def get(self, request):
         apps = ReleaseApp.objects.all().order_by('-created_at').first()
-        apps.count += 1
+        
+        if not apps:
+            return Response({'error': 'No apps found'}, status=status.HTTP_404_NOT_FOUND)
+
+        apps.count += 1  
         apps.save()
-        return Response({'url':apps.app}, status=status.HTTP_200_OK)
+        
+        return Response({'url': apps.app.url}, status=status.HTTP_200_OK)
