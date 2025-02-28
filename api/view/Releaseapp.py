9aef24adc6148a0e7fdbeeff5455666a3ffc68b6
snigdho48 <www.atik48@gmail.com>
2025-02-28 21:22:28 +0600

    Initial commit

--- api/view/Releaseapp.py
+++ api/view/Releaseapp.py
@@ -4,25 +4,24 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 
 from api.models import ReleaseApp
-import base64
-
-def encode_apk_to_base64(file_path):
-    with open(file_path, "rb") as apk_file:
-        encoded_string = base64.b64encode(apk_file.read()).decode('utf-8')
-    return encoded_string
-
+import os
 
 class ReleaseAppViewSet(APIView):
-    permission_classes = [AllowAny]  
+    permission_classes = [AllowAny]
 
     def get(self, request):
         apps = ReleaseApp.objects.all().order_by('-created_at').first()
-        
+
         if not apps:
             return Response({'error': 'No apps found'}, status=status.HTTP_404_NOT_FOUND)
 
-        apps.count += 1  
+        apps.count += 1
         apps.save()
-        apk = encode_apk_to_base64(apps.app.path)
-        
-        return Response({'base64': apk}, status=status.HTTP_200_OK)
+
+        file_path = apps.app.path
+        if not os.path.exists(file_path):
+            return Response({'error': 'APK file not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Return the APK file URL (adjust URL if needed based on your media file serving configuration)
+        file_url = f'https://ec2-3-108-205-134.ap-south-1.compute.amazonaws.com/media/{apps.app.name}.apk'
+        return Response({'url': file_url}, status=status.HTTP_200_OK)
