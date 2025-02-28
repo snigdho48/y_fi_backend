9aa526832030a692d72e00a90bbc50c678c6b3c8
snigdho48 <www.atik48@gmail.com>
2025-02-28 15:29:00 +0600

    Initial commit

--- y_fi_backend/settings.py
+++ y_fi_backend/settings.py
@@ -162,12 +162,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Different from STATICFILES_DIRS
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/assets',  # Only include actual static assets
+    BASE_DIR / 'staticfiles/',
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
