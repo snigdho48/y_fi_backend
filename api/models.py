e2b7b1ed5f71a6bd41d57dd8aceb09c13ae97ba2
Atiquzzaman Snigdho <www.atik48@gmail.com>
2026-05-14 11:41:30 +0600

    updated db ,migrate models & created sup user

--- api/models.py
+++ api/models.py
@@ -1,21 +1,46 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils import timezone
 import pytz
 
 def get_current_time():
     return timezone.now().astimezone(pytz.timezone('Asia/Dhaka'))
 
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
     id = models.AutoField(primary_key=True, editable=False)  # Unique primary key
     username = models.CharField(max_length=150, unique=False,null=True,blank=True)  # Not unique
     email = models.EmailField(blank=True, null=True,unique=True)  # Not unique
-    USERNAME_FIELD = 'email'  # Authenticate using username
-    REQUIRED_FIELDS = []  # Required fields for user creation
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
+        return self.username or self.email or str(self.pk)
 
 
 class UserProfile(models.Model):
